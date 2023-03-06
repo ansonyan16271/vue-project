1168a76cb5cd645adf9f2e89ab1f0c46295898fe
ansonyan16271 <ansonyan16271@163.com>
2023-03-06 19:56:41 +0800

    图表

---
+++ src/views/Home/index/components/chart.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <div ref="chart" style="width:100%;height:300px"></div>
+    </div>
+  </template>
+  
+  <script>
+  import * as echarts from 'echarts' 
+  
+  export default {
+    //props:['type'],//父传子
+    props:{
+        type:{
+            type:String,
+            default:'bar'
+        }
+    },
+    data(){
+      return {
+        option:{
+                title:{
+                    text:'echart图表'
+                },
+                xAxis:{
+                    data:['1月','2月','3月','4月','5月','6月']
+                },
+                yAxis:{},
+                series:[
+                    {
+                        type:this.type,
+                        data:[10,20,30,40,50,60]
+                    }
+                ]
+            }
+      }
+    },
+    mounted(){
+        var myChart = echarts.init(this.$refs.chart);
+         myChart.setOption(this.option);
+         //响应式
+         window.addEventListener('resize',()=>{
+            myChart.resize(); 
+         })
+    },
+  }
+  </script>
+  
+  
+  
--- src/views/Home/index/index.vue
+++ src/views/Home/index/index.vue
@@ -1,19 +1,56 @@
 <template>
-    <div>
-      企业首页
+  <div class="content">
+    <!-- 一行二列   -->
+    <div class="content-row">
+      <el-row :gutter="10">
+        <el-col :xs="24" :sm="24" :md="8" :lg="8">
+          <div class="bg-white bg-purple"><Chart :type="'bar'" /></div>
+        </el-col>
+        <el-col :xs="24" :sm="24" :md="16" :lg="16">
+          <div class="bg-white bg-purple-light"><Chart :type="'line'" /></div>
+        </el-col>
+      </el-row>
     </div>
-  </template>
+    <!-- 平均分配  -->
+    <div class="content-row">
+      <el-row :gutter="10">
+        <el-col :xs="24" :sm="24" :md="12" :lg="12">
+          <div class="bg-white bg-purple"><Chart /></div>
+        </el-col>
+        <el-col :xs="24" :sm="24" :md="12" :lg="12">
+          <div class="bg-white bg-purple-light"><Chart /></div>
+        </el-col>
+      </el-row>
+    </div>
+
+    <!-- 二行二列 -->
+    <div class="content-row">
+      <el-row :gutter="10">
+        <el-col :xs="24" :sm="24" :md="12" :lg="12">
+          <div class="bg-purple">
+            <div class="content-row">
+              <el-row>
+                <el-col :span="24">
+                  <div class="bg-white bg-purple">11111</div>
+                </el-col>
+              </el-row>
+            </div>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
   
   <script>
-  import { UserInfo } from '@/api/api';
-  export default {
-    data(){
-      return {
-  
-      }
-    },
-    methods:{
-  
-    }
-  }
-  </script>
+import { Chart } from "./components";
+export default {
+  components: {
+    Chart,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
